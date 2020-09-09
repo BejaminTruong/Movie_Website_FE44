68ecbf1ac47a3bc0bba823c05c92f75f78072616
truong thai binh <thaibinh291201@gmail.com>
2020-09-09 08:03:27 +0700

    add Logo to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,50 @@
 import React from "react";
-import {Row, Col, Space} from "antd";
-import "./Header.scss"
-
-export const Header= () => {
+import { Row, Col, Space } from "antd";
+import "./Header.scss";
+import saitama from "../../images/icons8-saitama-48.png";
+export const Header = () => {
   return (
     <section className="header">
       <nav className="navbar__wrapper">
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify="space-between" align="middle">
+        <Row
+          gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+          justify="space-between"
+          align="middle"
+        >
           <Col span={6}>
-            <div>col-6</div>
+            <div>
+              <img src={saitama} alt="saitama" />
+            </div>
           </Col>
           <Col span={10}>
-            <Row justify="center" >
+            <Row justify="center">
               <Space size={20}>
-                <a className="nav__link" href="#">Trang Chủ</a>                       
-                <a className="nav__link" href="#">Liên Hệ</a>                      
-                <a className="nav__link" href="#">Tin Tức</a>                   
-                <a className="nav__link" href="#">Ứng Dụng</a>                    
+                <a className="nav__link" href="#">
+                  Trang Chủ
+                </a>
+                <a className="nav__link" href="#">
+                  Liên Hệ
+                </a>
+                <a className="nav__link" href="#">
+                  Tin Tức
+                </a>
+                <a className="nav__link" href="#">
+                  Ứng Dụng
+                </a>
               </Space>
             </Row>
           </Col>
-          <Col span={8}>    
+          <Col span={8}>
             <Row justify="end">
               <Space>
-                <button className="btn ant-btn-background-ghost">Đăng Ký</button>                        
-                <button className="btn ant-btn-background-ghost">Đăng Nhập</button>       
-            </Space>
-            </Row>                           
+                <button className="btn ant-btn-background-ghost">
+                  Đăng Ký
+                </button>
+                <button className="btn ant-btn-background-ghost">
+                  Đăng Nhập
+                </button>
+              </Space>
+            </Row>
           </Col>
         </Row>
       </nav>
