584d9aa624afe11ee5e5810a00a2ecb4936c66e0
truong thai binh <thaibinh291201@gmail.com>
2020-09-10 13:36:17 +0700

    add backTop

--- src/templates/HomeTemplate/HomeTemplate.js
+++ src/templates/HomeTemplate/HomeTemplate.js
@@ -2,11 +2,14 @@ import React,{Fragment} from 'react'
 import {Route} from 'react-router-dom'
 import { Header } from '../../components/Header/Header'
 import { Footer } from '../../components/Footer/Footer'
+import { BackTop } from 'antd';
+import "./HomeTemplate.scss"
 const HomeComponent = (props) =>{
     return <Fragment>
         <Header />
         {props.children}
         <Footer/>
+        <BackTop className="backTop"/>
     </Fragment>
 }
 
