b75feacf1b3274e4b27788dd32ad869f9f6d0b5d
Khong Huu Toan <khonghuutoan@gmail.com>
2020-09-15 00:52:15 +0700

    Homepage finished

--- src/components/Cinema/Cinema.jsx
+++ src/components/Cinema/Cinema.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Row, Col, Space } from "antd";
 import {ListCards} from "components/ListCards/ListCards"
+import moment from "moment"
 import SwiperCore,{Scrollbar,Lazy,Mousewheel} from "swiper";
 import "swiper/swiper.scss"
 import "./Cinema.scss"
@@ -9,7 +10,7 @@ SwiperCore.use([Scrollbar,Lazy,Mousewheel])
 
 export const Cinema = (props) => {
 
-const { DSHeThongRap, DSCumRap, DSLichChieu, handleSetDSCumRap, handleSetDSLichChieu } = props;
+const { DSHeThongRap, DSCumRap, DSLichChieu, handleSetDSCumRap } = props;
 let [maHeThongRap,setMaHeThongRap] = useState(0);
 let [maCumRap,setMaCumRap] = useState(0);
 
@@ -29,14 +30,13 @@ const renderHeThongRap = () =>{
 }
 
 const renderCumRap = () =>{
-    return <ListCards data={DSCumRap} scrollbar breakpoints={{1200:{slidesPerView: 6},992:{slidesPerView: 6},768:{slidesPerView: 6},576 :{slidesPerView: 6}}} lazy direction="vertical" slidesPerColumnFill="column" slidesPerGroup={1}
+    return <ListCards data={DSCumRap} scrollbar={{draggable:true}} breakpoints={{1200:{slidesPerView: 6},992:{slidesPerView: 6},768:{slidesPerView: 6},576 :{slidesPerView: 6}}} lazy direction="vertical" slidesPerColumnFill="column" slidesPerGroup={1}
     tag="div" mousewheel >
         {(cumrap,index) =>{
 
             const active = index === maCumRap ? "active" : "";
 
             return <div className={`cinema__branch ${active}`} key={index} onClick={()=>{
-                    handleSetDSLichChieu(cumrap.maHeThongRap,cumrap.maCumRap);
                     setMaCumRap(index);
                 }}>
                 <div className="cinema__branch__img">
@@ -53,33 +53,45 @@ const renderCumRap = () =>{
  } 
 
  const renderLichChieu = () =>{
-    return <ListCards data={DSLichChieu} scrollbar lazy slidesPerView={4} direction="vertical" slidesPerGroup={1} freeMode="true"
-    slidesPerColumnFill="column" 
-    tag="div" >
-        {(phim) =>{
-            return <div className="cinema__schedule">
-                <div className="cinema__branch__img">
-                    <img src={phim.hinhAnh} alt={phim.tenPhim} width="20%" height="100%" />
-                    <div className="cinema__branch__info">
-                    <h3>{phim.tenPhim}</h3>
+
+    const LichChieu = DSLichChieu?.find(item => item.maCumRap === DSCumRap[maCumRap].maCumRap);
+
+    if(LichChieu){
+        return <ListCards data={LichChieu.danhSachPhim} scrollbar={{draggable:true,dragSize:"auto"}} lazy direction="vertical" slidesPerColumnFill="column" slidesPerGroup={1}
+        tag="div" mousewheel >
+            {(phim,index) =>{
+                return <div className="cinema__schedule" key={index}>
+                    <div className="cinema__schedule__img">
+                        <img src={phim.hinhAnh} alt={phim.tenPhim} />
+                        <div className="cinema__schedule__content">
+                            <h3>{phim.tenPhim}</h3>
+                            <div className="cinema__schedule_info">
+                                {phim.lstLichChieuTheoPhim.map((item,index)=>{
+                                    return <div key={index} className="schedule__second">
+                                        <span>{moment(item.ngayChieuGioChieu).format('hh:mm A')}</span>
+                                    </div>
+                                })}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-        }}
-    </ListCards>
+            }}
+        </ListCards>
+    }
+    return <></>
  }
 
   return <section className="cinema">
       <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
-          <Col span={3} style={{padding:0}}>
+          <Col span={3}>
               <Space direction="vertical" size="large" style={{width:"100%"}}>
                 {renderHeThongRap()}
               </Space> 
           </Col>
-          <Col span={10}>
+          <Col span={9}>
               {renderCumRap()}
           </Col>
-          <Col span={11}>
+          <Col span={12}>
             {renderLichChieu()}
           </Col>
       </Row>
